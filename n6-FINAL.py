910e2b343fe0227b0e0a8219235724927d238f2d
Delta35-DRC <126948223+Delta35-DRC@users.noreply.github.com>
2023-04-22 11:33:50 +0200

    final
    -

--- n6-FINAL.py
+++ n6-FINAL.py
@@ -2,13 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-connection = sqlite3.connect('table410w.sl3', 5)
+connection = sqlite3.connect('table250v.db', 5)
 cur = connection.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS data (times TEXT, value REAL);')
+cur.execute('CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, value TEXT);')
 
+quote = input('search: ')
+html = requests.get(f'https://www.bing.com/search?q={quote}')
+soup = BeautifulSoup(html.content, 'lxml')
+url = soup.select('cite')
+for value in url:
+    print(value)
+    cur.execute('INSERT INTO data (value) VALUES (?);', (value,))
 
-def search():
-    quote = input()
-    html = requests.get(f'https://www.bing.com/search?q={quote}')
-    soup = BeautifulSoup(html.content, 'lxml')
-    url = soup.find('.b_algo, cite')
+connection.commit()
+cur.execute('SELECT value, COUNT(*) AS count FROM data GROUP BY value ORDER BY count DESC')
+rows = cur.fetchall()
+connection.commit()
+connection.close()
